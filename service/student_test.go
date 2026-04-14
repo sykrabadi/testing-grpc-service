0536555c239d730a93bf6c3198ab41e46ffd48bd
Syukur Abadi <64145699+sykrabadi@users.noreply.github.com>
2026-04-14 10:51:49 +0700

    chore: add GetStudentV1 test to introduce failing test

--- service/student_test.go
+++ service/student_test.go
@@ -12,6 +12,15 @@ func TestGetStudent(t *testing.T){
 	svcProxy := newServiceProxy()
 
 	t.Run("success get student", func(t *testing.T) {
+		resp, err := svcProxy.svcV1.GetStudentV1(cctx, "",1)
+
+		// will return error, because test is relying to actual gRPC connection, 
+		// but no gRPC server is live currently
+		assert.NoError(t, err)
+		assert.Equal(t, 1, resp)
+	})
+
+	t.Run("success get student", func(t *testing.T) {
 		resp, err := svcProxy.svc.GetStudent(cctx, 1)
 		assert.NoError(t, err)
 		assert.Equal(t, 1, resp)
